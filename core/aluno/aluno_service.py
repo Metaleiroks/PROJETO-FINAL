a69a3d65bbe5155a236ecde97a1906550b8b5a3e
Metaleiroks <kevynpaulo7@gmail.com>
2025-10-16 21:39:13 -0300

    Erro corrigido obj reposity

--- core/aluno/aluno_service.py
+++ core/aluno/aluno_service.py
@@ -2,8 +2,8 @@ from core.aluno.aluno_repository import AlunoRepository
 from core.aluno.aluno import Aluno
 
 class AlunoService:
-    def init__(self):
-        self.repository = AlunoRepository
+    def __init__(self):
+        self.repository = AlunoRepository()
 
     def listar_alunos(self):    
         return self.repository.listar()
